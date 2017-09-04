2e499a21339dc231ae9f60ef64b5891d61fd0f1d
Soorya <sooryaprakash13@gmail.com>
2017-09-04 13:19:11 +0530

    String and Number functions

---
+++ number functions.js
@@ -0,0 +1,9 @@
+//as executed in the command prompt
+
+var num=3;
+num.toExponential();
+num.toFixed(2);
+num.toPrecision(3);
+var c=0.1+0.2;
+c;//0.30000000000000004
+num.toString();
---
+++ string functions.js
@@ -0,0 +1,17 @@
+//as excuted in browser prompt
+var str='JavaScript';
+var stro=new String('JavaScript');
+str.toUpperCase();
+str.toLowerCase();
+str.charAt(3);
+str.charCodeAt(3);
+str.indexOf('a');
+str.lastIndexOf('a');
+str.search('a');//this will allow regex
+str.slice(1,3);
+str.substring(1,3);
+str.substr(1,3);
+str.split(',');
+str.trim();
+str.toUpperCase();
+str.toLowerCase();
